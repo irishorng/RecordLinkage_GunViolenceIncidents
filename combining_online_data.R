8b4be73c903dde11ab82cf2ead66b73a8f8f36dd
irishorng <52606833+irishorng@users.noreply.github.com>
2025-06-08 11:42:42 -0400

    Update combining_online_data.R
    user friendly

--- combining_online_data.R
+++ combining_online_data.R
@@ -13,18 +13,38 @@ library("stringr")
 ########## put in file paths for all the csv files that you want to use
 ########## from the online GVA records
 ################################################################################
-data1<-read.csv("childrenkilled.csv",header=TRUE,sep=",",fill=TRUE) # better to name it with the meaning "childrenkilled" instead of the numbers 1 ,2 or something
-data2<-read.csv("officerinvolvedshooting.csv",header=TRUE,sep=",",fill=TRUE)
-# data3<-read.csv("school shootings.csv",header=TRUE,sep=",",fill=TRUE) #data3 didn't have any observations in 2014-2018
-# data4<-read.csv("teenskilled.csv",header=TRUE,sep=",",fill=TRUE) #data4 didn't have any observations in 2014-2018
-data5<-read.csv("mass_shootings_2014.csv",header=TRUE,sep=",",fill=TRUE)
-data6<-read.csv("mass_shootings_2015.csv",header=TRUE,sep=",",fill=TRUE)
-data7<-read.csv("mass_shootings_2016.csv",header=TRUE,sep=",",fill=TRUE)
-data8<-read.csv("mass_shootings_2017.csv",header=TRUE,sep=",",fill=TRUE)
-data9<-read.csv("mass_shootings_2018.csv",header=TRUE,sep=",",fill=TRUE)
-# data10<-read.csv("massshooting_allyears.csv",header=TRUE,sep=",",fill=TRUE) #data10 didn't have any observations in 2014-2018
 
+# set relative path
+current_dir = getwd()
+parent_dir = dirname(getwd())
+parent_parent_dir = dirname(parent_dir)
 
+create_path_name <- function(file, current_dir) {
+  return(paste(c(current_dir, file), collapse = "/"))
+}
+
+## the file paths of your GVA and NVDRS data
+childrenkilled_path <- create_path_name("childrenkilled.csv", current_dir)
+officerinvolvedshooting_path <- create_path_name("officerinvolvedshooting.csv", current_dir)
+# schoolshooting_path <- create_path_name("school shootings.csv", current_dir)
+# teenskilled_path <- create_path_name("teenskilled.csv", current_dir)
+mass_shootings_2014_path <- create_path_name("mass_shootings_2014.csv", current_dir)
+mass_shootings_2015_path <- create_path_name("mass_shootings_2015.csv", current_dir)
+mass_shootings_2016_path <- create_path_name("mass_shootings_2016.csv", current_dir)
+mass_shootings_2017_path <- create_path_name("mass_shootings_2017.csv", current_dir)
+mass_shootings_2018_path <- create_path_name("mass_shootings_2018.csv", current_dir)
+# mass_shootings_allyears_path <- create_path_name("mass_shooting_allyears.csv", current_dir)
+
+childrenkilled<-read.csv(childrenkilled_path,header=TRUE,sep=",",fill=TRUE) 
+officerinvolved<-read.csv(officerinvolvedshooting_path,header=TRUE,sep=",",fill=TRUE)
+# schoolshooting<-read.csv(schoolshooting_path,header=TRUE,sep=",",fill=TRUE) #schoolshooting didn't have any observations in 2014-2018
+# teenskilled<-read.csv(teenskilled_path,header=TRUE,sep=",",fill=TRUE) #teenskilled didn't have any observations in 2014-2018
+mass_shootings_2014<-read.csv(mass_shootings_2014_path,header=TRUE,sep=",",fill=TRUE)
+mass_shootings_2015<-read.csv(mass_shootings_2015_path,header=TRUE,sep=",",fill=TRUE)
+mass_shootings_2016<-read.csv(mass_shootings_2016_path,header=TRUE,sep=",",fill=TRUE)
+mass_shootings_2017<-read.csv(mass_shootings_2017_path,header=TRUE,sep=",",fill=TRUE)
+mass_shootings_2018<-read.csv(mass_shootings_2018_path,header=TRUE,sep=",",fill=TRUE)
+# mass_shootings_allyears<-read.csv(mass_shooting_allyears_path,header=TRUE,sep=",",fill=TRUE) #mass_shootings_allyears didn't have any observations in 2014-2018
 
 
 ################################################################################
@@ -38,49 +58,49 @@ states_2017 <- c('Alaska', 'Arizona', 'California', 'Colorado', 'Connecticut', '
 states_2018 <- c('Alabama', 'Alaska', 'Arizona', 'Colorado', 'Connecticut', 'Delaware', 'Georgia', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Missouri', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Rhode Island', 'South Carolina', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'District of Columbia', 'California', 'Illinois', 'Pennsylvania')
 
 ## str_sub is a function from the 'stringr' package
-data1_fix <- data1[(str_sub(data1$Incident.Date,-4,-1) == "2014" & data1$State %in% states_2014) |
-                     (str_sub(data1$Incident.Date,-4,-1) == "2015" & data1$State %in% states_2015 ) |
-                     (str_sub(data1$Incident.Date,-4,-1) == "2016" & data1$State %in% states_2016) |
-                     (str_sub(data1$Incident.Date,-4,-1) == "2017" & data1$State %in% states_2017) |
-                     (str_sub(data1$Incident.Date,-4,-1) == "2018" & data1$State %in% states_2018), ]
-data1_fix_kill <- data1_fix[data1_fix$X..Killed > 0, ]
-data1 <- data1_fix_kill
+childrenkilled_fix <- childrenkilled[(str_sub(childrenkilled$Incident.Date,-4,-1) == "2014" & childrenkilled$State %in% states_2014) |
+                     (str_sub(childrenkilled$Incident.Date,-4,-1) == "2015" & childrenkilled$State %in% states_2015 ) |
+                     (str_sub(childrenkilled$Incident.Date,-4,-1) == "2016" & childrenkilled$State %in% states_2016) |
+                     (str_sub(childrenkilled$Incident.Date,-4,-1) == "2017" & childrenkilled$State %in% states_2017) |
+                     (str_sub(childrenkilled$Incident.Date,-4,-1) == "2018" & childrenkilled$State %in% states_2018), ]
+childrenkilled_fix_kill <- childrenkilled_fix[childrenkilled_fix$X..Killed > 0, ]
+childrenkilled <- childrenkilled_fix_kill
 
-data2_fix <- data2[(str_sub(data2$Incident.Date,-4,-1) == "2016" & data2$State %in% states_2016), ]
-data2_fix_kill <- data2_fix[data2_fix$X..Killed > 0, ]
-data2 <- data2_fix_kill
+officerinvolved_fix <- officerinvolved[(str_sub(officerinvolved$Incident.Date,-4,-1) == "2016" & officerinvolved$State %in% states_2016), ]
+officerinvolved_fix_kill <- officerinvolved_fix[officerinvolved_fix$X..Killed > 0, ]
+officerinvolved <- officerinvolved_fix_kill
 
-data5_fix <- data5[(str_sub(data5$Incident.Date,-4,-1) == "2014" & data5$State %in% states_2014), ]
-data5_fix_kill <- data5_fix[data5_fix$X..Victims.Killed > 0 | data5_fix$X..Subjects.Suspects.Killed > 0 , ]
-data5 <- data5_fix_kill
+mass_shootings_2014_fix <- mass_shootings_2014[(str_sub(mass_shootings_2014$Incident.Date,-4,-1) == "2014" & mass_shootings_2014$State %in% states_2014), ]
+mass_shootings_2014_fix_kill <- mass_shootings_2014_fix[mass_shootings_2014_fix$X..Victims.Killed > 0 | mass_shootings_2014_fix$X..Subjects.Suspects.Killed > 0 , ]
+mass_shootings_2014 <- mass_shootings_2014_fix_kill
 
-data6_fix <- data6[(str_sub(data6$Incident.Date,-4,-1) == "2015" & data6$State %in% states_2015), ]
-data6_fix_kill <- data6_fix[data6_fix$X..Victims.Killed > 0 | data6_fix$X..Subjects.Suspects.Killed > 0 , ]
-data6 <- data6_fix_kill
+mass_shootings_2015_fix <- mass_shootings_2015[(str_sub(mass_shootings_2015$Incident.Date,-4,-1) == "2015" & mass_shootings_2015$State %in% states_2015), ]
+mass_shootings_2015_fix_kill <- mass_shootings_2015_fix[mass_shootings_2015_fix$X..Victims.Killed > 0 | mass_shootings_2015_fix$X..Subjects.Suspects.Killed > 0 , ]
+mass_shootings_2015 <- mass_shootings_2015_fix_kill
 
-data7_fix <- data7[(str_sub(data7$Incident.Date,-4,-1) == "2016" & data7$State %in% states_2016), ]
-data7_fix_kill <- data7_fix[data7_fix$X..Killed > 0 , ]
-data7 <- data7_fix_kill
+mass_shootings_2016_fix <- mass_shootings_2016[(str_sub(mass_shootings_2016$Incident.Date,-4,-1) == "2016" & mass_shootings_2016$State %in% states_2016), ]
+mass_shootings_2016_fix_kill <- mass_shootings_2016_fix[mass_shootings_2016_fix$X..Killed > 0 , ]
+mass_shootings_2016 <- mass_shootings_2016_fix_kill
 
-data8_fix <- data8[(str_sub(data8$Incident.Date,-4,-1) == "2017" & data8$State %in% states_2017), ]
-data8_fix_kill <- data8_fix[data8_fix$X..Victims.Killed > 0 | data8_fix$X..Subjects.Suspects.Killed > 0 , ]
-data8 <- data8_fix_kill
+mass_shootings_2017_fix <- mass_shootings_2017[(str_sub(mass_shootings_2017$Incident.Date,-4,-1) == "2017" & mass_shootings_2017$State %in% states_2017), ]
+mass_shootings_2017_fix_kill <- mass_shootings_2017_fix[mass_shootings_2017_fix$X..Victims.Killed > 0 | mass_shootings_2017_fix$X..Subjects.Suspects.Killed > 0 , ]
+mass_shootings_2017 <- mass_shootings_2017_fix_kill
 
-data9_fix <- data9[(str_sub(data9$Incident.Date,-4,-1) == "2018" & data9$State %in% states_2018), ]
-data9_fix_kill <- data9_fix[data9_fix$X..Victims.Killed > 0 | data9_fix$X..Subjects.Suspects.Killed > 0 , ]
-data9 <- data9_fix_kill
+mass_shootings_2018_fix <- mass_shootings_2018[(str_sub(mass_shootings_2018$Incident.Date,-4,-1) == "2018" & mass_shootings_2018$State %in% states_2018), ]
+mass_shootings_2018_fix_kill <- mass_shootings_2018_fix[mass_shootings_2018_fix$X..Victims.Killed > 0 | mass_shootings_2018_fix$X..Subjects.Suspects.Killed > 0 , ]
+mass_shootings_2018 <- mass_shootings_2018_fix_kill
 
 
 ## merging all the data together into one file
-data_merged<-bind_rows(data1,data2)
-# data_merged2<-bind_rows(data_merged,data3)
-# data_merged3<-bind_rows(data_merged2,data4)
-data_merged4<-bind_rows(data_merged,data5)
-data_merged5<-bind_rows(data_merged4,data6)
-data_merged6<-bind_rows(data_merged5,data7)
-data_merged7<-bind_rows(data_merged6,data8)
-data_merged8<-bind_rows(data_merged7,data9)
-# data_merged9<-bind_rows(data_merged8,data10)
+data_merged<-bind_rows(childrenkilled,officerinvolved)
+# data_merged2<-bind_rows(data_merged,schoolshooting)
+# data_merged3<-bind_rows(data_merged2,teenskilled)
+data_merged4<-bind_rows(data_merged,mass_shootings_2014)
+data_merged5<-bind_rows(data_merged4,mass_shootings_2015)
+data_merged6<-bind_rows(data_merged5,mass_shootings_2016)
+data_merged7<-bind_rows(data_merged6,mass_shootings_2017)
+data_merged8<-bind_rows(data_merged7,mass_shootings_2018)
+# data_merged9<-bind_rows(data_merged8,mass_shootings_allyears)
 
 GVA_online_combined <- data_merged8
 
